4f6dff1cec7a72f81b48f1665bc84bf28f461ac1
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-12 22:09:13 +0300

    created url paths for the login, registration and index templates

--- awwards/urls.py
+++ awwards/urls.py
@@ -5,10 +5,14 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('project_details/<int:project_id>', views.project_details, name='project-details'),
     path('profile/',profile_list),
     path('profile/<int:id>',profile_detail),
     path('project/',project_list),
+    path('project/<int:id>',project_detail),
     path('register/', views.register_request, name='register'),
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name= "logout"),
+    path("add_project/", views.new_project, name= "add_project"),
+
 ]
