99113f4175f67999061e1fb0e236f03b5f201ce7
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-12 22:11:18 +0300

    added static paths for media files in the settings.py

--- devawwards/settings.py
+++ devawwards/settings.py
@@ -131,5 +131,5 @@ os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
