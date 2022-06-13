6304a31b39dab57611760781fe58878c78c17b84
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-13 18:59:27 +0300

    model migration update

--- devawwards/settings.py
+++ devawwards/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static'),
+os.path.join(BASE_DIR, 'awwards/static'),
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
