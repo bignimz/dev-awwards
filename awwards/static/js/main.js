0a701f599c49d7a566d5a6a750e73abe86901c4f
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-13 18:44:24 +0300

    added backround image to hero section

---
+++ awwards/static/js/main.js
@@ -0,0 +1,11 @@
+// const cardBody = document.getElementById('devproj');
+
+document.querySelector('devaw-card').onmouseover = function () {
+    alert('_hoveredItem')
+}
+
+// $(document).ready(function() {
+//     $('.devaw-card').hover(function(){
+//         $('.devaw-body').show();
+//     })
+// })
